2f7e815f82f39070a64ef7aedd9b6c0c1fac9ab3
Aksanti Bahiga tacite <71480535+marcellintacite@users.noreply.github.com>
2023-02-23 10:34:09 +0300

    Adding some pages

--- src/pages/Maternite/data/Article.js
+++ src/pages/Maternite/data/Article.js
@@ -1,6 +1,8 @@
 import doctor_consultation_02 from "../Assets/doctor_consultation_02.png";
 import Maskgroup from "../Assets/Mask_group.png";
 import mask from "../Assets/mask.png";
+import mama from "../Assets/4263290.jpg";
+import min from "../Assets/6457326-min.jpg";
 
 export const Articles = [
   {
@@ -10,13 +12,13 @@ export const Articles = [
     HeureLecture: "2 min de lecture",
   },
   {
-    image: Maskgroup,
+    image: mama,
     titre: "Comment utiliser SOS Doctor avec efficacité 2",
     date: "12 février 2023",
     HeureLecture: "2 min de lecture",
   },
   {
-    image: doctor_consultation_02,
+    image: min,
     titre: "Comment utiliser SOS doctor avec efficacité 3",
     date: "12 février 2023",
     HeureLecture: "2 min de lecture",
--- src/pages/Secours/data/Article.js
+++ src/pages/Secours/data/Article.js
@@ -3,41 +3,45 @@ import Maskgroup from "../Assets/Mask_group.png";
 import mask from "../Assets/mask.png";
 import i1 from "../Assets/3144362140_1_6_pqYiIr30.jpg";
 import iw from "../Assets/images.png";
+import ambulance from "../Assets/3809187.jpg";
+import respi from "../Assets/artificial-respiration-first-aid-help-concept.jpg";
+import doctor from "../Assets/doctor-providing-first-aid.jpg";
+import avion from "../Assets/tcik_gbyf_220226-min.jpg";
 
 export const Articles = [
   {
+    image: ambulance,
+    titre: "Trouver un hopital proche de chez vous",
+    date: "12 février 2023",
+    HeureLecture: "1 min de lecture",
+  },
+  {
+    image: respi,
+    titre: "Problème de respiration ? on en parle",
+    date: "12 février 2023",
+    HeureLecture: "2 min de lecture",
+  },
+  {
+    image: doctor,
+    titre: "Comment utiliser SOS doctor avec efficacité 3",
+    date: "12 février 2023",
+    HeureLecture: "2 min de lecture",
+  },
+  {
+    image: avion,
+    titre: "Transporetz un handicap dans un avion",
+    date: "12 février 2023",
+    HeureLecture: "2 min de lecture",
+  },
+  {
+    image: doctor_consultation_02,
+    titre: "Comment utiliser SOS doctor avec efficacité 3",
+    date: "12 février 2023",
+    HeureLecture: "2 min de lecture",
+  },
+  {
     image: iw,
-    titre: "Arreter une hémoragie externe",
-    date: "12 février 2023",
-    HeureLecture: "2 min de lecture",
-  },
-  {
-    image: Maskgroup,
-    titre: "Comment utiliser SOS Doctor avec efficacité 2",
-    date: "12 février 2023",
-    HeureLecture: "2 min de lecture",
-  },
-  {
-    image: doctor_consultation_02,
-    titre: "Comment utiliser SOS doctor avec efficacité 3",
-    date: "12 février 2023",
-    HeureLecture: "2 min de lecture",
-  },
-  {
-    image: i1,
-    titre: "Comment utiliser SOS doctor avec efficacité 3",
-    date: "12 février 2023",
-    HeureLecture: "2 min de lecture",
-  },
-  {
-    image: doctor_consultation_02,
-    titre: "Comment utiliser SOS doctor avec efficacité 3",
-    date: "12 février 2023",
-    HeureLecture: "2 min de lecture",
-  },
-  {
-    image: doctor_consultation_02,
-    titre: "Comment utiliser SOS doctor avec efficacité 3",
+    titre: "Premiers gestes qui sauvent",
     date: "12 février 2023",
     HeureLecture: "2 min de lecture",
   },
